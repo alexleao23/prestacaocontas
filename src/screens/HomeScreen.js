e44569cb2c9a4867895cfa7b73d69c2b77ecb83b
Alexander Leão <alexandersilvadeleao23@gmail.com>
2018-12-08 15:45:52 -0300

    Initial Commit

---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,14 @@
+import React, { Component }from 'react';
+import { View, Text } from 'react-native';
+
+class HomeScreen extends Component {
+    render() {
+        return (
+            <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+                <Text>Home Screen</Text>
+            </View>
+        );
+    }
+}
+
+export default HomeScreen;
